52f72e8820100a7bb75463a3a4640845e17b44c2
vgrem <vvgrem@gmail.com>
2026-05-20 21:54:36 +0300

    refactor: convert ReportBase to @dataclass

--- office365/sharepoint/search/reports/base.py
+++ office365/sharepoint/search/reports/base.py
@@ -1,9 +1,13 @@
+from __future__ import annotations
+
+from dataclasses import dataclass
+
 from office365.runtime.client_value import ClientValue
 
 
+@dataclass
 class ReportBase(ClientValue):
-    def __init__(self, farm_id=None):
-        self.FarmId = farm_id
+    FarmId: str | None = None
 
     @property
     def entity_type_name(self):
