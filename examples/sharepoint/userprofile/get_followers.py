788112786a8e17e5c9340144cc2aa74dd4ea7371
vgrem <vvgrem@gmail.com>
2026-05-24 02:07:46 +0300

    Fix ruff lint issues in notebooks: magic numbers, builtin shadowing

--- examples/sharepoint/userprofile/get_followers.py
+++ examples/sharepoint/userprofile/get_followers.py
@@ -4,7 +4,7 @@ https://learn.microsoft.com/en-us/sharepoint/dev/apis/people-rest-api
 """
 
 from office365.sharepoint.client_context import ClientContext
-from tests import test_client_id, test_password, test_site_url, test_tenant, test_username, test_user_principal_name
+from tests import test_client_id, test_password, test_site_url, test_tenant, test_user_principal_name, test_username
 
 ctx = ClientContext(test_site_url).with_username_and_password(
     tenant=test_tenant,
