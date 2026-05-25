381504750c84af671993a4f9615cf778553f0897
vgrem <vvgrem@gmail.com>
2026-05-26 01:29:14 +0300

    Switch download_from_lib.py from with_client_secret to with_username_and_password

--- examples/sharepoint/files/download_from_lib.py
+++ examples/sharepoint/files/download_from_lib.py
@@ -8,9 +8,14 @@ import tempfile
 from pathlib import Path
 
 from office365.sharepoint.client_context import ClientContext
-from tests import test_client_id, test_client_secret, test_site_url, test_tenant
+from tests import test_client_id, test_password, test_site_url, test_tenant, test_username
 
-ctx = ClientContext(test_site_url).with_client_secret(test_tenant, test_client_id, test_client_secret)
+ctx = ClientContext(test_site_url).with_username_and_password(
+    tenant=test_tenant,
+    client_id=test_client_id,
+    username=test_username,
+    password=test_password,
+)
 
 doc_lib = ctx.web.lists.get_by_title("Documents")
 items = (
