fe90c5dc044df056a4fce0ce0d5752dadb1a9462
vgrem <vvgrem@gmail.com>
2026-05-25 22:54:50 +0300

    Add additionalDetails to signin_logs output

--- examples/sharepoint/audit/signin_logs.py
+++ examples/sharepoint/audit/signin_logs.py
@@ -10,4 +10,7 @@ from tests import test_client_id, test_client_secret, test_tenant
 client = GraphClient(tenant=test_tenant).with_client_secret(test_client_id, test_client_secret)
 signins = client.audit_logs.signins.top(10).get().execute_query()
 for s in signins:
-    print(f"{s.created_datetime}: {s.user_display_name} (errorCode={s.status.error_code}, failureReason='{s.status.failure_reason}')")
+    print(f"{s.created_datetime}: {s.user_display_name} "
+          f"(errorCode={s.status.error_code}, "
+          f"failureReason='{s.status.failure_reason}', "
+          f"additionalDetails='{s.status.additional_details}')")
