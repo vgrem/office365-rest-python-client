78ddb3eb4d02f176d4f97a5c5d338c1ecd89d5aa
vgrem <vvgrem@gmail.com>
2026-05-31 00:42:47 +0300

    test: add @requires_delegated to admin tests
    - test1 (SharePoint settings): SharePointTenantSettings.Read.All
      + SharePoint Administrator / GA bypass
    - test2 (update settings): SharePointTenantSettings.ReadWrite.All
      + SharePoint Administrator / GA bypass
    - test3 (service issues): ServiceMessage.Read.All,
      ServiceHealth.Read.All + Service Support Admin / GA / GR
    - test4 (M365 apps): OrgSettings.Read.All + Reports Admin /
      GA / GR

--- tests/admin/test_admin.py
+++ tests/admin/test_admin.py
@@ -1,22 +1,39 @@
+from tests.decorators import requires_delegated
 from tests.graph_case import GraphDelegatedTestCase
 
 
 class TestAdmin(GraphDelegatedTestCase):
     """SharePoint specific test case base class"""
 
+    @requires_delegated(
+        "SharePointTenantSettings.Read.All",
+        bypass_roles=["SharePoint Administrator", "Global Administrator"],
+    )
     def test1_get_sharepoint_settings(self):
         result = self.client.admin.sharepoint.settings.get().execute_query()
         self.assertIsNotNone(result.resource_path)
 
+    @requires_delegated(
+        "SharePointTenantSettings.ReadWrite.All",
+        bypass_roles=["SharePoint Administrator", "Global Administrator"],
+    )
     def test2_update_sharepoint_settings(self):
         settings = self.client.admin.sharepoint.settings
         settings.sharing_blocked_domain_list = ["contoso.com", "fabrikam.com"]
         settings.update().execute_query()
 
+    @requires_delegated(
+        "ServiceMessage.Read.All", "ServiceHealth.Read.All",
+        bypass_roles=["Service Support Administrator", "Global Administrator", "Global Reader"],
+    )
     def test3_list_issues(self):
         result = self.client.admin.service_announcement.issues.get().execute_query()
         self.assertIsNotNone(result.resource_path)
 
+    @requires_delegated(
+        "OrgSettings.Read.All",
+        bypass_roles=["Reports Administrator", "Global Administrator", "Global Reader"],
+    )
     def test4_list_microsoft365_apps(self):
         result = self.client.admin.microsoft365_apps.get().execute_query()
         self.assertIsNotNone(result.resource_path)
