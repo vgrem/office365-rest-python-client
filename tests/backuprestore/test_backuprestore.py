64af789d1a1ea511440d80972a224aaef491205b
vgrem <vvgrem@gmail.com>
2026-05-31 00:49:12 +0300

    test: add @requires_delegated to backup restore tests
    - test1: BackupRestore.ReadWrite.All (enable/write)
    - test2: BackupRestore.Read.All (read)
    - bypass: Global Administrator

--- tests/backuprestore/test_backuprestore.py
+++ tests/backuprestore/test_backuprestore.py
@@ -1,12 +1,15 @@
+from tests.decorators import requires_delegated
 from tests.graph_case import GraphDelegatedTestCase
 
 
 class TestBackupRestore(GraphDelegatedTestCase):
+    @requires_delegated("BackupRestore.ReadWrite.All", bypass_roles=["Global Administrator"])
     def test1_enable_backup_restore(self):
         tenant_id = "af6a80a4-8b4b-4879-88af-42ff8a545211"
         result = self.client.solutions.backup_restore.enable(tenant_id).execute_query()
         self.assertIsNotNone(result.value)
 
+    @requires_delegated("BackupRestore.Read.All", bypass_roles=["Global Administrator"])
     def test2_get_backup_restore(self):
         result = self.client.solutions.backup_restore.get().execute_query()
         self.assertIsNotNone(result.resource_path)
