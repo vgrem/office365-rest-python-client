2fc7ebd505bc8df33d3311367d1b6c8de75911df
vgrem <vvgrem@gmail.com>
2026-05-31 13:26:01 +0300

    fix: update hunting query example to use AlertInfo table
    DeviceProcessEvents not available in all tenants.
    AlertInfo is a core Microsoft 365 Defender table
    available in most E5 tenants.

--- examples/security/run_hunting_query.py
+++ examples/security/run_hunting_query.py
@@ -1,11 +1,10 @@
 """
 This example specifies a KQL query which does the following:
 
-   - Looks into the DeviceProcessEvents table in the advanced hunting schema.
-   - Filters on the condition that the event is initiated by the powershell.exe process.
-   - Specifies the output of 3 columns from the same table for each row: Timestamp, FileName, InitiatingProcessFileName.
-   - Sorts the output by the Timestamp value.
-   - Limits the output to 2 records (2 rows)
+   - Looks into the AlertInfo table in the advanced hunting schema.
+   - Filters on alerts with high severity.
+   - Projects selected columns.
+   - Limits the output to 2 records.
 
 https://learn.microsoft.com/en-us/graph/api/resources/security-api-overview
 https://learn.microsoft.com/en-us/graph/api/security-security-runhuntingquery?view=graph-rest-1.0
@@ -16,10 +15,10 @@ from tests import test_client_id, test_client_secret, test_tenant
 
 client = GraphClient(tenant=test_tenant).with_client_secret(test_client_id, test_client_secret)
 query = """
-DeviceProcessEvents
-| where InitiatingProcessFileName =~ \"powershell.exe\"
-| project Timestamp, FileName, InitiatingProcessFileName
-| order by Timestamp desc | limit 2"""
+AlertInfo
+| where Severity =~ "High"
+| project Timestamp, AlertId, Title, Severity
+| limit 2"""
 
 result = client.security.run_hunting_query(query).execute_query()
 print(result.value)
