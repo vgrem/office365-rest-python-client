957a83102115f4ee303706bdccfb2659c0c861a1
Vadim Gremyachev <vvgrem@gmail.com>
2026-06-02 10:57:15 +0000

    docs: add README and 4 new scripts for groups (list, get_by_name, add/remove user)

---
+++ examples/sharepoint/groups/add_user_to_group.py
@@ -0,0 +1,17 @@
+"""Demonstrates how to add a user to a SharePoint group.
+
+Official documentation: https://learn.microsoft.com/en-us/sharepoint/dev/apis/rest-api/csom/group
+"""
+
+from office365.sharepoint.client_context import ClientContext
+from tests import test_client_id, test_password, test_site_url, test_tenant, test_username
+
+ctx = ClientContext(test_site_url).with_username_and_password(
+    tenant=test_tenant,
+    client_id=test_client_id,
+    username=test_username,
+    password=test_password,
+)
+group = ctx.web.site_groups.get_by_name("Team Site Members")
+user = group.users.add_user("i:0#.f|membership|user@contoso.com").execute_query()
+print(f"User added: {user.title}")
